8813d1aedb0c5cb1445f21acedecceb9c8376497
Josman Proudinat Silva <josman@localhost.localdomain>
2015-06-25 13:53:42 -0500

    Modificación a los settings

--- herokudjango/settings.py
+++ herokudjango/settings.py
@@ -63,9 +63,9 @@ DATABASES['default'] = dj_database_url.config()
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
