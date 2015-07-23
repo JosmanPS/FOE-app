988f944827818f9c6587695cbb41214af4705b98
Josman Proudinat Silva <josman@localhost.localdomain>
2015-07-23 02:35:05 -0500

    Cambios a los templates para recibir media

--- herokudjango/settings.py
+++ herokudjango/settings.py
@@ -26,6 +26,16 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+)
+
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
